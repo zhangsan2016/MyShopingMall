18077521d3926359850303eca81463a617203a04
liu <602694077@qq.com>
2017-07-31 16:10:09 +0800

    2017年7月31日 16:10:05

---
+++ app/src/main/java/com/ldgd/com/myshopingmall/home/adapter/HomeRecycleAdapter.java
@@ -0,0 +1,34 @@
+package com.ldgd.com.myshopingmall.home.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+import com.ldgd.com.myshopingmall.home.bean.TypeListBean;
+
+/**
+ * Created by ldgd on 2017/7/31.
+ */
+
+public class HomeRecycleAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    public HomeRecycleAdapter(Context mContext, TypeListBean.ResultBean resultBean) {
+
+    }
+
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 1;
+    }
+}
